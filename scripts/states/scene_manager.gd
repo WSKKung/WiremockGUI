2aae88ae7cab2d022565f7392c6d5aa6caf95d79
sahachai.p <sahachai.p@orbitdigital.co.th>
2025-11-28 13:46:20 +0700

    fix: return page loop

--- scripts/states/scene_manager.gd
+++ scripts/states/scene_manager.gd
@@ -7,6 +7,7 @@ func change_scene_to_node(scene: Node) -> void:
 	var old_scene_path = old_scene.scene_file_path
 	if old_scene_path != "":
 		last_open_scene_paths.append(old_scene_path)
+	
 	get_tree().root.add_child(scene)
 	get_tree().current_scene = scene
 	old_scene.queue_free()
@@ -16,4 +17,8 @@ func change_scene_to_previus() -> void:
 	if scene_path == null:
 		return
 	var scene = load(scene_path).instantiate()
-	change_scene_to_node(scene)
+	var old_scene = get_tree().current_scene
+	
+	get_tree().root.add_child(scene)
+	get_tree().current_scene = scene
+	old_scene.queue_free()
